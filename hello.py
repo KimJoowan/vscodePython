24d59e1f24c5607d17de3a3a9c12440aac4d290f
kimjoowan <a01055136572@gamil.com>
2022-11-21 11:47:21 +0900

    git commit "1 commit"

---
+++ hello.py
@@ -0,0 +1,24 @@
+print('hello')
+
+# 데이터 형
+# 변수
+# 데이터 구조(리스트, 딕셔너리ㅡ튜플,집합)
+# 제어문(조건문, 반복문)
+# 클래스함수
+# 외부라이브러리 사용 (테이터 분석)(통계)
+# 사각화
+
+a = 10
+b = 20
+c = '파이썬'
+print(a+b)
+print(type(c))
+
+
+
+num1 = int(input('input num1:'))
+num2 = int(input('input num2:'))
+
+print(f'{num1}+{num2} = {num1 + num2}')
+
+
