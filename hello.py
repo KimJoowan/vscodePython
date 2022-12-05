0e01007bf8403ade765164aa2679412f0070fe7a
kimjoowan <a01055136572@gamil.com>
2022-12-05 12:21:28 +0900

    53445

--- hello.py
+++ hello.py
@@ -1,24 +1,25 @@
 print('hello')
 
-# 데이터 형
-# 변수
-# 데이터 구조(리스트, 딕셔너리ㅡ튜플,집합)
+# 데이터 형/변수
+# 데이터 구조(리스트, 딕셔너리,튜플,집합)
 # 제어문(조건문, 반복문)
-# 클래스함수
-# 외부라이브러리 사용 (테이터 분석)(통계)
-# 사각화
+# 클래스/함수
+# 외부라이브러리 사용 (테이터 분석 =pandas)(통계=numpy)
+# 사각화(matplotblic4)
 
 a = 10
 b = 20
 c = '파이썬'
+
 print(a+b)
 print(type(c))
-
-
 
 num1 = int(input('input num1:'))
 num2 = int(input('input num2:'))
 
 print(f'{num1} + {num2} = {num1 + num2}')
+
+for i in range(1,11):
+    print(i)
 
 
